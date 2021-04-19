b471d72758b202602a8f217c1c6c238a30791b2e
NCRanjbar <70384054+NCRanjbar@users.noreply.github.com>
2021-04-19 02:01:22 +0200

    connectScenes+1

---
+++ SonsOfAnonymous/Assets/Scenes/ExitGametoMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ExitGametoMenu : MonoBehaviour
+{
+    public void SceneSwitcher()
+    {
+        Application.LoadLevel(0);
+    }
+}
---
+++ SonsOfAnonymous/Assets/Scenes/SceneSwitch1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneSwitch1 : MonoBehaviour
+{
+  public void ScenSwitcher()
+    {
+        SceneManager.LoadScene(1);
+    }
+  
+        
+    
+}
