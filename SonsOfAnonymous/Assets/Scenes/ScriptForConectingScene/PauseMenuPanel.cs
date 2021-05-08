8630f236d4e1b95e9f72f4c4ac2527a2be23b80b
NCRanjbar <70384054+NCRanjbar@users.noreply.github.com>
2021-05-09 00:45:32 +0200

    PANEL2

---
+++ SonsOfAnonymous/Assets/Scenes/ScriptForConectingScene/PauseMenuPanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuPanel : MonoBehaviour
+{
+    public static bool GameIsPaused = true;
+    public GameObject pauseMenuUI;
+
+
+    
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+       
+            if (GameIsPaused)
+            {
+           
+            Resume();
+            }
+         
+        
+        
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+   
+}
---
+++ SonsOfAnonymous/Assets/Scenes/ScriptForConectingScene/SceneSwitchContinue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneSwitchContinue : MonoBehaviour
+{
+    public void ScenSwitcher()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
+        Time.timeScale = 0f;
+    }
+
+
+
+}
