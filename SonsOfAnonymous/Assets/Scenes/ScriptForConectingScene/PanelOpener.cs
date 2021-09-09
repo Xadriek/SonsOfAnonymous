9f5ccfe9e06610dc5c17c7fa1af7ecccccdc3688
Fox <72220905+Xadriek@users.noreply.github.com>
2021-09-09 14:12:02 +0200

    fixCountDown

--- SonsOfAnonymous/Assets/Scenes/ScriptForConectingScene/PanelOpener.cs
+++ SonsOfAnonymous/Assets/Scenes/ScriptForConectingScene/PanelOpener.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class PanelOpener : MonoBehaviour
 {
     public GameObject Panel;
+    public static bool GameIsPaused = false;
+
     // Start is called before the first frame update
-  public void OpenPanel()
+    public void OpenPanel()
     {
         if (Panel != null)
         {
             Panel.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
         }
     }
 }
