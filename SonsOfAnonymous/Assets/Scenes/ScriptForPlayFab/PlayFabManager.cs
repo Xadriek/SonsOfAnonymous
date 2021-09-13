2ecb8d8687ef2a94dd193bca09eba1c26e3b1b5f
ensranjbar <55926968+ensranjbar@users.noreply.github.com>
2021-09-13 21:19:38 +0200

    playfab...

--- SonsOfAnonymous/Assets/Scenes/ScriptForPlayFab/PlayFabManager.cs
+++ SonsOfAnonymous/Assets/Scenes/ScriptForPlayFab/PlayFabManager.cs
@@ -1,9 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Newtonsoft.Json;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+
+
+
 public class PlayFabManager : MonoBehaviour
 {
-  
-    
+
+    [Header("UI")]
+    public Text massageText;
+    public InputField emailInput;
+    public InputField passwordInput;
+
+    public void RegisterButton()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = false
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+    }
+
+    void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        massageText.text = "Registered and logged in!!!";
+    }
+
+
+     void Start()
+    {
+        Login();
+
+    }
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Successful login/account create!");
+    }
+
+    void OnError(PlayFabError error)
+    {
+        Debug.Log("Error while logging in/creating account!");
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+
 }
