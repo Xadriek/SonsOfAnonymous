6edd165ef02539c58047eff476f0a6314c601509
Fox <72220905+Xadriek@users.noreply.github.com>
2021-09-18 00:40:03 +0200

    buildTest

---
+++ SonsOfAnonymous/Assets/Points.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour
+{
+    public Text pointsText;
+    public void AddPoints()
+    {
+        scoring.totalScore += 10;
+    }
+    public void GetPoints()
+    {
+        int recievedPoints =0;
+        pointsText.text = recievedPoints.ToString();
+    }
+}
